8cc2371cdde898aef00a78027a5667aa70842f3e
wb-xzq536241 <wb-xzq536241@alibaba-inc.com>
2019-03-27 11:33:30 +0800

    test1分支

--- src/main/java/com/springapp/mvc/HelloController.java
+++ src/main/java/com/springapp/mvc/HelloController.java
@@ -1,18 +1,8 @@
 package com.springapp.mvc;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.ModelMap;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
 
-@Controller
-@RequestMapping("/")
 public class HelloController {
-    @RequestMapping(method = RequestMethod.GET)
-    @ResponseBody
-    public String printWelcome() {
-    
-        return "bbbxzq_dev";
+    public void printWelcome() {
+        System.out.println("hellow");
     }
 }
